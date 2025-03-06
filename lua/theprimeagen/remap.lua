36a0b9f6b5fb955609f9839f41d9daad6af503eb
Mykhailo Bortnyk <legendmiha115@gmail.com>
2025-03-06 10:20:37 +0000

    Select whole file hotkey

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- select whole file
+vim.keymap.set("n", "<C-a>", "<esc>gg0VG<cr>")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
