20a60b3e97f7e0be9c4d747419125a098ba81518
Mykhailo Bortnyk <legendmiha115@gmail.com>
2025-03-10 15:34:51 +0000

    small fixes

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -4,11 +4,16 @@ return {
     tag = "0.1.5",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons"
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = { "node_modules", ".git" },
+            }
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
